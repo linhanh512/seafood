0c53b06314774d4200a7b43b779e2659587cfdb1
Linh Anh <annielin512@gmail.com>
2019-05-13 19:27:02 +0700

    haha

---
+++ src/model/OrderRow.java
@@ -0,0 +1,230 @@
+package model;
+
+import domainapp.basics.exceptions.ConstraintViolationException;
+import domainapp.basics.model.meta.AttrRef;
+import domainapp.basics.model.meta.DAssoc;
+import domainapp.basics.model.meta.DAttr;
+import domainapp.basics.model.meta.DClass;
+import domainapp.basics.model.meta.DOpt;
+import domainapp.basics.model.meta.DAssoc.AssocEndType;
+import domainapp.basics.model.meta.DAssoc.AssocType;
+import domainapp.basics.model.meta.DAssoc.Associate;
+import domainapp.basics.model.meta.DAttr.Type;
+import domainapp.basics.util.Tuple;
+import domainapp.basics.util.cache.StateHistory;
+import model.Seafood;
+import model.OrderTable;
+
+/**
+ * @overview an order Rows give each row in orderList in SeafoodBill
+ * 
+ * @author Do Thi Thuy Linh
+ *
+ */
+@DClass(schema="seafoodman")
+public class OrderRow implements Comparable{
+	
+	public static final String A_id="id";
+	public static final String A_seafood="seafood";
+	public static final String A_number="numberInKilo";
+	public static final String A_pricePerKilo="pricePerKilo";
+	public static final String A_price="price";
+	public static final String A_bill="bill";
+	
+	//attributes
+	@DAttr(name=A_id,id=true,auto=true,type=Type.Integer,length=3,mutable=false,optional=false)
+	private int id;
+	private static int idCounter = 0;
+	
+	@DAttr(name=A_seafood,type=Type.Domain,optional=false,length = 6)
+	@DAssoc(ascName="order-has-seafood",role="order",
+	ascType=AssocType.One2One, endType=AssocEndType.One,
+	associate=@Associate(type=Seafood.class,cardMin=1,cardMax=1))
+	private Seafood seafood;
+	
+	@DAttr(name=A_number,type=Type.Double,length=4,optional=false)
+	private Double numberInKilo;
+	
+	@DAttr(name=A_pricePerKilo,type=Type.Double,length=6,optional=false)
+	private Double pricePerKilo;
+	
+	@DAttr(name=A_price,auto=true, type=Type.Double,length=6,optional=true, mutable=false,
+			serialisable=false,derivedFrom= {A_pricePerKilo,A_number})
+	private Double price;	
+	
+	private StateHistory<String, Object> stateHist;
+	
+	@DAttr(name="table",type=Type.Domain,optional=true)
+	@DAssoc(ascName="table-has-orders",role="order",
+	ascType=AssocType.One2One, endType=AssocEndType.One,
+	associate=@Associate(type=OrderTable.class,cardMin=1,cardMax=1))
+	private OrderTable table;
+	
+	//constructor
+	@DOpt(type=DOpt.Type.DataSourceConstructor)
+	public OrderRow(Integer id, Seafood seafood,Double pricePerKilo, Double numberInKilo,
+			Double price, OrderTable table) throws ConstraintViolationException{
+		this.id = nextId(id);
+		this.seafood = seafood;
+		this.table = table;
+		this.pricePerKilo = (pricePerKilo != null)? pricePerKilo.doubleValue():null;
+		this.numberInKilo = (numberInKilo != null)? numberInKilo.doubleValue():null;
+		stateHist = new StateHistory<>();
+		updateFinalPrice();
+	}
+	
+	@DOpt(type=DOpt.Type.DataSourceConstructor)
+	public OrderRow(Integer id, Seafood seafood,Double pricePerKilo, Double numberInKilo,OrderTable table) {
+		this(id,seafood,pricePerKilo,numberInKilo,null,table);
+	}
+	
+	@DOpt(type=DOpt.Type.ObjectFormConstructor)
+	public OrderRow(Seafood seafood, Double pricePerKilo, Double numberInKilo, OrderTable table) {
+		this(null,seafood,pricePerKilo,numberInKilo,null,table);
+	}
+	
+	@DOpt(type=DOpt.Type.ObjectFormConstructor)
+	@DOpt(type=DOpt.Type.RequiredConstructor)
+	public OrderRow(Seafood seafood,OrderTable table) throws ConstraintViolationException {
+		this(null,seafood,0.0,0.0,null,table);
+	}
+	
+	//setter
+	public void setSeafood(Seafood seafood) {
+		this.seafood = seafood;
+	}
+
+	public void setPricePerKilo(Double price) {
+		setPricePerKilo(price,false);
+	}
+	
+	public void setTable(OrderTable table) {
+		this.table = table;
+	}
+	
+	public void setPricePerKilo(Double price,boolean updateFinalPrice) {
+		this.pricePerKilo = price;
+		if(updateFinalPrice)
+			updateFinalPrice();
+	}
+	
+	public void setNumberInKilo(Double number) {
+		setNumberInKilo(number,false);
+	}
+	
+	public void setNumberInKilo(Double number,boolean updateFinalPrice) {
+		this.numberInKilo = number;
+		if(updateFinalPrice)
+			updateFinalPrice();
+	}
+	
+	@DOpt(type=DOpt.Type.DerivedAttributeUpdater)
+	@AttrRef(value=A_price)
+	private void updateFinalPrice() {
+		if(!numberInKilo.equals(null) && !pricePerKilo.equals(null)) {
+			price = numberInKilo * pricePerKilo;
+			
+			stateHist.put(A_price, price);
+		}
+	}
+	//getter
+	public int getId() {
+		return id;
+	}
+	
+	public double getPricePerKilo() {
+		return pricePerKilo;
+	}
+	
+	public double getNumberInKilo() {
+		return numberInKilo;
+	}
+	
+	public double getPrice() {
+		return getPrice(false);
+	}
+	
+	public double getPrice(boolean cached) throws IllegalStateException {
+		if(cached) {
+			Object val = stateHist.get(A_price);
+			
+			if(val == null)
+				throw new IllegalStateException(
+						"OrderRow.getPrice: cached value is null");
+			
+			return (Double) val;
+			
+		}else {
+			if(price != null)
+				return price;
+			else
+				return 0.0;
+		}
+	}
+	
+	public Seafood getSeafood() {
+		return seafood;
+	}
+	
+	public OrderTable getTable() {
+		return table;
+	}
+	
+	private Integer nextId(Integer currID) throws ConstraintViolationException{
+	    if (currID == null) {
+	      idCounter++;
+	      return idCounter;
+	    } else {
+	      int num = currID.intValue();
+	      if (num > idCounter)
+	        idCounter = num;
+	      
+	      return currID;
+	    }
+	  }
+
+	  /**
+	   * @requires 
+	   *  minVal != null /\ maxVal != null
+	   * @effects 
+	   *  update the auto-generated value of attribute <tt>attrib</tt>, specified for <tt>derivingValue</tt>, using <tt>minVal, maxVal</tt>
+	   */
+	  @DOpt(type=DOpt.Type.AutoAttributeValueSynchroniser)
+	  public static void updateAutoGeneratedValue(
+	      DAttr attrib,
+	      Tuple derivingValue, 
+	      Object minVal, 
+	      Object maxVal) throws ConstraintViolationException {
+	    
+	    if (minVal != null && maxVal != null) {
+	      int maxIdVal = (Integer) maxVal;
+	      if (maxIdVal > idCounter)  
+	        idCounter = maxIdVal;
+	    }
+	  }
+	  
+	  /**
+		* @effects returns <code>this.id</code>
+		*/
+		@Override
+		public String toString() {
+			return toString(true);
+		}
+
+		public String toString(boolean full) {
+			if (full)
+				return "Order(" + id + ", "+ seafood.toString(false) +", "+ numberInKilo +", "+price+")";
+		    else
+		    	return "Order(" + id + ")";
+		}
+
+		@Override
+		public int compareTo(Object o) {
+			if (o == null || (!(o instanceof OrderRow)))
+			      return -1;
+
+			    OrderRow e = (OrderRow) o;
+
+			    return this.seafood.getId().compareTo(e.seafood.getId());
+		}
+}
---
+++ src/model/OrderTable.java
@@ -0,0 +1,217 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import domainapp.basics.exceptions.ConstraintViolationException;
+import domainapp.basics.model.meta.AttrRef;
+import domainapp.basics.model.meta.DAssoc;
+import domainapp.basics.model.meta.DAttr;
+import domainapp.basics.model.meta.DClass;
+import domainapp.basics.model.meta.DOpt;
+import domainapp.basics.model.meta.Select;
+import domainapp.basics.model.meta.DAssoc.AssocEndType;
+import domainapp.basics.model.meta.DAssoc.AssocType;
+import domainapp.basics.model.meta.DAssoc.Associate;
+import domainapp.basics.model.meta.DAttr.Type;
+import domainapp.basics.util.Tuple;
+
+/**
+ * @overview an order table contains various of order rows
+ * 
+ * @author Do Thi Thuy Linh
+ *
+ */
+@DClass(schema="seafoodman")
+public class OrderTable {
+	@DAttr(name="id",id=true,auto=true,length=6,mutable=false,type=Type.Integer)
+	  private int id;
+	  private static int idCounter;
+	  
+	  @DAttr(name="name",length=20,type=Type.String,optional=false)
+	  private String name;
+	  
+	  @DAttr(name="OrderRow",type=Type.Collection,
+	      serialisable=false,optional=false,
+	      filter=@Select(clazz=OrderRow.class))
+	  @DAssoc(ascName="Table-has-OrderRow",role="table",
+	      ascType=AssocType.One2Many,endType=AssocEndType.One,
+	      associate=@Associate(type=OrderRow.class,
+	      cardMin=1,cardMax=25))  
+	  private Collection<OrderRow> OrderRow;
+	  
+	  // derived attributes
+	  private int OrderRowCount;
+	  
+	  @DOpt(type=DOpt.Type.ObjectFormConstructor)
+	  @DOpt(type=DOpt.Type.RequiredConstructor)
+	  public OrderTable(@AttrRef("name") String name) {
+	    this(null, name);
+	  }
+
+	  // constructor to create objects from data source
+	  @DOpt(type=DOpt.Type.DataSourceConstructor)
+	  public OrderTable(@AttrRef("id") Integer id,@AttrRef("name") String name) {
+	    this.id = nextID(id);
+	    this.name = name;
+	    
+	    OrderRow = new ArrayList<>();
+	    OrderRowCount = 0;
+	  }
+
+	  @DOpt(type=DOpt.Type.Setter)
+	  public void setName(String name) {
+	    this.name = name;
+	  }
+
+	  @DOpt(type=DOpt.Type.LinkAdder)
+	  //only need to do this for reflexive association: @MemberRef(name="OrderRow")  
+	  public boolean addOrderRow(OrderRow s) {
+	    if (!this.OrderRow.contains(s)) {
+	      OrderRow.add(s);
+	    }
+	    
+	    // no other attributes changed
+	    return false; 
+	  }
+
+	  @DOpt(type=DOpt.Type.LinkAdderNew)
+	  public boolean addNewOrderRow(OrderRow s) {
+	    OrderRow.add(s);
+	    OrderRowCount++;
+	    
+	    // no other attributes changed
+	    return false; 
+	  }
+	  
+	  @DOpt(type=DOpt.Type.LinkAdder)
+	  public boolean addOrderRow(Collection<OrderRow> OrderRow) {
+	    for (OrderRow s : OrderRow) {
+	      if (!this.OrderRow.contains(s)) {
+	        this.OrderRow.add(s);
+	      }
+	    }
+	    
+	    // no other attributes changed
+	    return false; 
+	  }
+
+	  @DOpt(type=DOpt.Type.LinkAdderNew)
+	  public boolean addNewOrderRow(Collection<OrderRow> OrderRow) {
+	    this.OrderRow.addAll(OrderRow);
+	    OrderRowCount += OrderRow.size();
+
+	    // no other attributes changed
+	    return false; 
+	  }
+
+	  @DOpt(type=DOpt.Type.LinkRemover)
+	  //only need to do this for reflexive association: @MemberRef(name="OrderRow")
+	  public boolean removeOrderRow(OrderRow s) {
+	    boolean removed = OrderRow.remove(s);
+	    
+	    if (removed) {
+	      OrderRowCount--;
+	    }
+	    
+	    // no other attributes changed
+	    return false; 
+	  }
+	  
+	  @DOpt(type=DOpt.Type.Setter)
+	  public void setOrderRow(Collection<OrderRow> OrderRow) {
+	    this.OrderRow = OrderRow;
+	    
+	    OrderRowCount = OrderRow.size();
+	  }
+	    
+	  /**
+	   * @effects 
+	   *  return <tt>OrderRowCount</tt>
+	   */
+	  @DOpt(type=DOpt.Type.LinkCountGetter)
+	  public Integer getOrderRowCount() {
+	    return OrderRowCount;
+	  }
+
+	  @DOpt(type=DOpt.Type.LinkCountSetter)
+	  public void setOrderRowCount(int count) {
+	    OrderRowCount = count;
+	  }
+	  
+	  @DOpt(type=DOpt.Type.Getter)
+	  public String getName() {
+	    return name;
+	  }
+	  
+	  @DOpt(type=DOpt.Type.Getter)
+	  public Collection<OrderRow> getOrderRow() {
+	    return OrderRow;
+	  }
+	  
+	  @DOpt(type=DOpt.Type.Getter)
+	  public int getId() {
+	    return id;
+	  }
+	  
+	  @Override
+	  public String toString() {
+	    return "OrderTable("+getId()+","+getName()+")";
+	  }
+	  
+	  @Override
+	  public int hashCode() {
+	    final int prime = 31;
+	    int result = 1;
+	    result = prime * result + id;
+	    return result;
+	  }
+
+	  @Override
+	  public boolean equals(Object obj) {
+	    if (this == obj)
+	      return true;
+	    if (obj == null)
+	      return false;
+	    if (getClass() != obj.getClass())
+	      return false;
+	    OrderTable other = (OrderTable) obj;
+	    if (id != other.id)
+	      return false;
+	    return true;
+	  }
+
+	  private static int nextID(Integer currID) {
+	    if (currID == null) {
+	      idCounter++;
+	      return idCounter;
+	    } else {
+	      int num = currID.intValue();
+	      if (num > idCounter)
+	        idCounter = num;
+	      
+	      return currID;
+	    }
+	  }
+
+	  /**
+	   * @requires 
+	   *  minVal != null /\ maxVal != null
+	   * @effects 
+	   *  update the auto-generated value of attribute <tt>attrib</tt>, specified for <tt>derivingValue</tt>, using <tt>minVal, maxVal</tt>
+	   */
+	  @DOpt(type=DOpt.Type.AutoAttributeValueSynchroniser)
+	  public static void updateAutoGeneratedValue(
+	      DAttr attrib,
+	      Tuple derivingValue, 
+	      Object minVal, 
+	      Object maxVal) throws ConstraintViolationException {
+	    
+	    if (minVal != null && maxVal != null) {
+	      if (attrib.name().equals("id")) {
+	        int maxIdVal = (Integer) maxVal;
+	        if (maxIdVal > idCounter)  
+	          idCounter = maxIdVal;
+	      }
+	    }
+	  }
+}
